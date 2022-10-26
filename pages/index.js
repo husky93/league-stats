e0e28036c74ca9d7ac804e5faf7a3276989b6b47
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-26 17:48:23 +0200

    Add Theme switch Button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import Page from '../layouts/Page';
+import { useColorMode } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import SearchBar from '../components/SearchBar';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { IconButton } from '@chakra-ui/button';
 
 export default function Home() {
-  return <div></div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Page>
+      <Head>
+        <title>Recipes Web App</title>
+        <meta name="description" content="Recipes Web App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <IconButton mt={4} aria-label="Toggle Mode" onClick={toggleColorMode}>
+          {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+        </IconButton>
+      </div>
+      <SearchBar />
+    </Page>
+  );
 }
