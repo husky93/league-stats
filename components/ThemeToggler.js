33faaa5cdebec2b1b665329dcef3f1b268f27758
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-26 18:01:37 +0200

    Move theme toggler to top-right corner

--- components/ThemeToggler.js
+++ components/ThemeToggler.js
@@ -6,7 +6,13 @@ import { IconButton } from '@chakra-ui/button';
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <IconButton mt={4} aria-label="Toggle Mode" onClick={toggleColorMode}>
+    <IconButton
+      mt={4}
+      position="fixed"
+      right="24px"
+      aria-label="Toggle Mode"
+      onClick={toggleColorMode}
+    >
       {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
     </IconButton>
   );
